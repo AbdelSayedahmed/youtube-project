766d048b7cc1f9e5304db79f5849a5559c8e88a8
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-09 17:33:45 -0400

    Styling complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import About from "/src/components/About.jsx";
-import Filter from "/src/components/Filter.jsx";
 import Home from "/src/components/Home.jsx";
 import Show from "/src/components/Show.jsx";
 import Nav from "/src/components/Nav.jsx";
 import Shows from "/src/components/Shows.jsx";
-import "/src/App.css";
+import "./App.css";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+  function handleFilter(input) {
+    setSearch(input);
+  }
+
   return (
     <>
-      <Nav />
-      <Filter />
+      <Nav handleFilter={handleFilter} />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/home/:search" element={<Shows />} />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
-import "/src/components/Nav.css";
+import Filter from "./Filter";
+import "./Nav.css";
 
-export default function Nav() {
+export default function Nav({ handleFilter }) {
   return (
     <div className="nav-container">
       <Link to="/home" className="nav-container_link">
         <img src="/src/assets/youtubelogo.png" alt="Home" id="youtubelogo" />
       </Link>
+      <Filter handleFilter={handleFilter} />
       <Link to="/About" className="nav-container_link">
         About
       </Link>

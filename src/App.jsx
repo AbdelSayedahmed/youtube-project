fbf9974718186de970c05acd461bbcab7a632da5
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-09 15:05:26 -0400

    Boilerplate created

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "/src/components/About.jsx";
+import Comments from "/src/components/Comments.jsx";
+import Filter from "/src/components/Filter.jsx";
+import Home from "/src/components/Home.jsx";
+import Show from "/src/components/Show.jsx";
+import "/src/App.css";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="" element={null} />
+      <Route path="" element={null} />
+      <Route path="" element={null} />
+      <Route path="" element={null} />
+      <Route path="" element={null} />
+    </Routes>
+  );
+}

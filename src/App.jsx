07f9a2d063182b8c783dfa1e09a94a807664ff06
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-09 15:16:09 -0400

    Created more necessary components. Created routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import About from "/src/components/About.jsx";
-import Comments from "/src/components/Comments.jsx";
 import Filter from "/src/components/Filter.jsx";
 import Home from "/src/components/Home.jsx";
 import Show from "/src/components/Show.jsx";
+import Nav from "/src/components/Nav.jsx";
+import Shows from "/src/components/Shows.jsx";
 import "/src/App.css";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="" element={null} />
-      <Route path="" element={null} />
-      <Route path="" element={null} />
-      <Route path="" element={null} />
-      <Route path="" element={null} />
-    </Routes>
+    <>
+      <Nav />
+      <Filter />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/:search" element={<Shows />} />
+        <Route path="/home/:search/:id" element={<Show />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }

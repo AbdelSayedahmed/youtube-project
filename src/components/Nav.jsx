a191d735ea1a6336626b52dcd2b178613899ca42
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-09 18:07:12 -0400

    revamped styling on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,13 +5,17 @@ import "./Nav.css";
 export default function Nav({ handleFilter }) {
   return (
     <div className="nav-container">
-      <Link to="/home" className="nav-container_link">
-        <img src="/src/assets/youtubelogo.png" alt="Home" id="youtubelogo" />
-      </Link>
-      <Filter handleFilter={handleFilter} />
-      <Link to="/About" className="nav-container_link">
-        About
-      </Link>
+      <div className="nav-container_left">
+        <Link to="/home" className="nav-container_link">
+          <img src="/src/assets/youtubelogo.png" alt="Home" id="youtubelogo" />
+        </Link>
+        <Filter handleFilter={handleFilter} />
+      </div>
+      <div className="nav-container_right">
+        <Link to="/About" className="nav-container_link">
+          About
+        </Link>
+      </div>
     </div>
   );
 }

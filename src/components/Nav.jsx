d8ed583891802bb451f04564ee20d246f97c2063
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-09 17:12:08 -0400

    radio

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <div className="nav-container">
       <Link to="/home" className="nav-container_link">
-        <img src="/src/assets/youtubelogo.png" alt="Home" id="youtubelogo"/>
+        <img src="/src/assets/youtubelogo.png" alt="Home" id="youtubelogo" />
       </Link>
       <Link to="/About" className="nav-container_link">
         About

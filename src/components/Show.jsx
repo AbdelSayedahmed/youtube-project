cac8fa58461272ce8da6b87090bfbfc37d67135e
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-11 10:55:03 -0400

    biolerplate created

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,3 +1,10 @@
+import Comments from "./Comments.jsx"
+
 export default function Show() {
-  return null;
+  return (
+    <div className="show-container">
+      <div className="show-container_video"></div>
+      <Comments />
+    </div>
+  );
 }

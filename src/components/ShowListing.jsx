ee4c1c651b7ae40396fd7282afdd568ce6d22b49
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-11 16:40:39 -0400

    styling complete

--- src/components/ShowListing.jsx
+++ src/components/ShowListing.jsx
@@ -3,9 +3,9 @@ import "./ShowListing.css";
 export default function ShowListing({ show }) {
   const { title, thumbnail } = show;
   return (
-    <>
+    <div className="listing-container">
       <img src={thumbnail} alt={`Thumbnail of ${title}`} />
       <h3>{title}</h3>
-    </>
+    </div>
   );
 }

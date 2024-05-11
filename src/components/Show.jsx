31043590625cf7a81fe7ac9f6a32141206217389
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-11 12:58:21 -0400

    created showlisting files

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,10 +1,3 @@
-import Comments from "./Comments.jsx"
-
 export default function Show() {
-  return (
-    <div className="show-container">
-      <div className="show-container_video"></div>
-      <Comments />
-    </div>
-  );
+  return null;
 }

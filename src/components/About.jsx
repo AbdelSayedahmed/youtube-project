c2baa5766aab618b0ce45796f49093ce66b4e971
Ari-Rosalie <collado.arr@gmail.com>
2024-05-15 20:45:02 -0400

    about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,42 @@
+import "./About.css"
+
 export default function About() {
-  return null;
+  return (
+    <div>
+      <div className="group-members">
+        <div className="group-member">
+          <img
+            src="../assets/IMG_1081.jpg"
+            alt="Shanelly"
+          />
+          <h3>Shanel</h3>
+          <p><a href="https://github.com/shanel-elm">GitHub</a></p>
+          <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod voluptatum quos tempore recusandae rerum debitis culpa eveniet voluptatem vitae, sed, eius esse fugit ipsam dolorum corrupti fuga, autem officiis quaerat?
+          </p>
+        </div>
+        <div className="group-member">
+          <img
+            src="https://ca.slack-edge.com/TCVA3PF24-U064KLDFYKB-1e41f20ca5e6-512"
+            alt="Staten Island King"
+          />
+          <h3>Abdelrahman Sayedahmed</h3>
+          <p><a href="https://github.com/AbdelSayedahmed">GitHub</a></p>
+          <p>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptatum pariatur sint commodi, enim rerum soluta distinctio totam ipsa labore incidunt quia omnis est. Maxime natus doloremque nam sed quis!
+          </p>
+        </div>
+        <div className="group-member">
+          <img
+            src="https://ca.slack-edge.com/TCVA3PF24-U02MFNE3HEU-87f17bade30c-512"
+            alt="just Ari"
+          />
+          <h3>Ari Collado</h3>
+          <p><a href="https://github.com/Ari-Rosalie">GitHub</a></p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident autem magni nihil neque, explicabo dicta sunt possimus suscipit maiores? Impedit vero dolorum aperiam blanditiis ipsam quos suscipit tempore quibusdam maiores.</p>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
